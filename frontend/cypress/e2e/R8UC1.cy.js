3a23d317f298070cb95e787bba4ca3b74cf0a486
OTTO <caeffrer@gmail.com>
2023-04-28 20:49:16 +0200

    Added fixture and cleanup

--- frontend/cypress/e2e/R8UC1.cy.js
+++ frontend/cypress/e2e/R8UC1.cy.js
@@ -1,28 +1,33 @@
-const newUser = "sampleuser@email.net";
-const todoItem = "Watch it again";
-
 describe("Logging in to system", () => {
-    beforeEach(function(){
-        cy.visit("localhost:3000");
-        cy.get("[id=email]").type(`${newUser}{enter}`);
-        
-        //Login or create the new user
-        cy.get('h1').then(($h1) => {
-            if($h1.text().includes("Your tasks,")){
-                cy.log("User already exists");
-            }else{
-                //Create a new user and a new task for that user
-                cy.contains("Have no account yet? Click here to sign up.").click();
-                cy.get("[id=email]").clear();
-                cy.get("[id=email]").type(`${newUser}{enter}`);
-                const newItem = "New task"
-                cy.get("[id=title]").type(`${newItem}`);
-                const itemUrl = "dQw4w9WgXcQ";
-                cy.get("[id=url]").type(`${itemUrl}{enter}`);
-            }
-            cy.contains("New task").click();
-        });
+    let uid
+    let email
+    const todoItem = "New Task"
+  
+    before(function () {
+      cy.fixture('example.json')
+        .then((user) => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:5000/users/create',
+                form: true,
+                body: user
+            }).then((response) => {
+                uid = response.body._id.$oid
+                email = user.email
+            })
+        })
     })
+
+    beforeEach(function() {
+        cy.visit("localhost:3000");
+        cy.get("[id=email]").type(`${email}{enter}`);
+        const newItem = "New task"
+        cy.get("[id=title]").type(`${newItem}`);
+        const itemUrl = "dQw4w9WgXcQ";
+        cy.get("[id=url]").type(`${itemUrl}{enter}`);
+    
+        cy.contains("New task").click();
+      })
     
     //Add a todo to the task
     it("Enter description into empty field", () => {
@@ -41,6 +46,15 @@ describe("Logging in to system", () => {
         cy.viewport(1024, 768);
         cy.get('input[placeholder*="Add a new todo item"]').click();
         cy.contains("Add").should('be.disabled');
+    })
+
+    after(function () {
+        cy.request({
+            method: 'DELETE',
+            url: `http://localhost:5000/users/${uid}`
+        }).then((response) => {
+            cy.log(response.body)
+        })
     })
 });
 
--- frontend/cypress/e2e/R8UC3.cy.js
+++ frontend/cypress/e2e/R8UC3.cy.js
@@ -1,29 +1,33 @@
-const newUser = "sampleuser@email.net";
-const todoItem = "Watch it again";
-const newItem = "New task";
-const itemUrl = "dQw4w9WgXcQ";
-
 describe("Logging in to system", () => {
-    beforeEach(function(){
+    let uid
+    let email
+    const todoItem = "New Task"
+  
+    before(function () {
+      cy.fixture('example.json')
+        .then((user) => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:5000/users/create',
+                form: true,
+                body: user
+            }).then((response) => {
+                uid = response.body._id.$oid
+                email = user.email
+            })
+        })
+    })
+
+    beforeEach(function() {
         cy.visit("localhost:3000");
-        cy.get("[id=email]").type(`${newUser}{enter}`);
-        
-        //Login or create the new user
-        cy.get('h1').then(($h1) => {
-            if($h1.text().includes("Your tasks,")){
-                cy.log("User already exists");
-            }else{
-                //Create a new user and a new task for that user
-                cy.contains("Have no account yet? Click here to sign up.").click();
-                cy.get("[id=email]").clear();
-                cy.get("[id=email]").type(`${newUser}{enter}`);
-                cy.get("[id=title]").type(`${newItem}`);
-                cy.get("[id=url]").type(`${itemUrl}{enter}`);
-            }
-            cy.viewport(1920, 1080);
-            cy.contains("New task").click();
+        cy.get("[id=email]").type(`${email}{enter}`);
+        const newItem = "New task"
+        cy.get("[id=title]").type(`${newItem}`);
+        const itemUrl = "dQw4w9WgXcQ";
+        cy.get("[id=url]").type(`${itemUrl}{enter}`);
+    
+        cy.contains("New task").click();
             cy.get('input[placeholder*="Add a new todo item"]').type(`${todoItem}{enter}`);
-        });
     })
 
     //Delete a todo from the task
@@ -35,4 +39,12 @@ describe("Logging in to system", () => {
         cy.contains('ul.todo-list', `${todoItem}`).should('not.exist'); 
     })
 
+    after(function () {
+        cy.request({
+            method: 'DELETE',
+            url: `http://localhost:5000/users/${uid}`
+        }).then((response) => {
+            cy.log(response.body)
+        })
+    })
 });
